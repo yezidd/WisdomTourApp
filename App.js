b059d3874f0479f3062cd78d17db95171b089a2c
yzdd <yjs10086>
2018-03-06 09:45:23 +0800

    初始化智慧旅游软件项目

--- App.js
+++ App.js
@@ -14,12 +14,11 @@ const instructions = Platform.select({
     "Shake or press menu button for dev menu"
 });
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.welcome}>Welcome to React Native!-------</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
