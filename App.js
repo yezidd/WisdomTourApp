cfb9189c11e24c4d9ec8b98affc23e21cfe4030c
yzdd <yjs10086>
2018-03-06 10:22:12 +0800

    初始化智慧旅游软件项目

--- App.js
+++ App.js
@@ -18,10 +18,14 @@ export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
+        <Text
+			style={styles.welcome}
+		>
 			Welcome to React Native!-------
 		</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.instructions}>
+			To get started, edit App.js
+		</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
     )
