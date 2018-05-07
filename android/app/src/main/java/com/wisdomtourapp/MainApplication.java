953cbb3429a2a1c435f0866bd5b684c768ac9fd4
yzdd <yjs10086>
2018-05-07 20:36:18 +0800

    初始化智慧旅游软件项目

--- android/app/src/main/java/com/wisdomtourapp/MainApplication.java
+++ android/app/src/main/java/com/wisdomtourapp/MainApplication.java
@@ -3,6 +3,7 @@ package com.wisdomtourapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import org.reactnative.camera.RNCameraPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
             return Arrays.<ReactPackage>asList(
                     new MainReactPackage(),
+            new RNCameraPackage(),
                     new AMap3DPackage()
             );
         }
